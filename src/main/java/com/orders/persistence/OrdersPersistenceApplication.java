3eb018983bf95b034428b10d6899ad870dd05afb
Pedro <pedro.canil@guatemaltek.com>
2025-10-06 16:23:09 -0600

    project created

---
+++ src/main/java/com/orders/persistence/OrdersPersistenceApplication.java
@@ -0,0 +1,13 @@
+package com.orders.persistence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersPersistenceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OrdersPersistenceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/orders/persistence/OrdersPersistenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orders.persistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersPersistenceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
