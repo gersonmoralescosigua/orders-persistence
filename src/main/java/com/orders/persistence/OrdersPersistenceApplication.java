917d5d6fe6e6c586f92a7dcf32ec2c6ba40723a2
gersonmoralescosigua <gersonmls240@gmail.com>
2025-10-06 21:43:29 -0600

    Mas Updates de subcarpetas dentro de src/main/

--- src/main/java/com/orders/persistence/OrdersPersistenceApplication.java
+++ src/main/java/com/orders/persistence/OrdersPersistenceApplication.java
@@ -1,13 +1,25 @@
+
+
 package com.orders.persistence;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OrdersPersistenceApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(OrdersPersistenceApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(OrdersPersistenceApplication.class, args);
+    }
+
+    @Bean
+    public Consumer<Documento> ingestPostgres(DocumentoService service) {
+        return doc -> {
+            service.save(doc);
+            // System.out.println("Guardado en Postgres: " + doc.getUniqueId());
+        };
+    }
+
 
 }
